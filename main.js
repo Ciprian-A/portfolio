929a671f186a89b027ca8fdfe04f114d5498c754
Ciprian <48722279+Ciprian-A@users.noreply.github.com>
2021-02-03 00:48:01 +0000

    Add weather app

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 AOS.init();
+const projectContainer = document.querySelectorAll('.card');
+// console.log('projectContainer', projectContainer)
+
+// const hoverFunc = () => {
+//   project.classList.toggle('large');
+// }
+// projectContainer.addEventListener('mouseout', hoverFunc);
+
+projectContainer.forEach(project=> {
+  project.addEventListener('mouseover', ()=> {
+    console.log('mouseover')
+    project.classList.toggle('large')
+  });
+  project.addEventListener('mouseout', ()=> {
+    console.log('mouseout')
+    project.classList.toggle('large')
+  });
+
+})
+
 
 
